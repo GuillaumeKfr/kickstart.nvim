3e772134841c6436515f73c007943a3ffed245eb
Orokh <guillaume.kaiffer@gmail.com>
2025-02-19 15:20:51 +0100

    feat: replace isort & black with ruff for python formatting

--- init.lua
+++ init.lua
@@ -761,7 +761,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
         json = { 'prettier' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
