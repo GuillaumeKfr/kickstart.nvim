bdc2e34f2bc1d912c3197d25f48ce62f00ee5723
Orokh <guillaume.kaiffer@gmail.com>
2025-03-19 15:37:11 +0100

    feat: add default tools for Mason

--- init.lua
+++ init.lua
@@ -706,6 +706,12 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'sqlfluff',
+        'ruff',
+        'ruff-lsp',
+        'shellcheck',
+        'shfmt',
+        'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
