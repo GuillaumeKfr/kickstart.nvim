787dfcb9a512d3fea4516a0e76633556b611f249
GuillaumeKfr <guillaume.kaiffer@sanofi.com>
2025-08-26 10:21:38 +0200

    fix: load WSL config only in WSL

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,14 +1,26 @@
-vim.g.clipboard = {
-  name = 'WSLClip',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-}
+file_exists = function(name)
+  local f = io.open(name, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+if file_exist '/etc/wsl.conf' then
+  vim.g.clipboard = {
+    name = 'WSLClip',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  }
+end
 
 -- Copy to system clipboard
 vim.keymap.set('n', '<leader>y', '"+y')
