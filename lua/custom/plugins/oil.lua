f8d24f32c48a5d727027a7501c65a45c9e11c7db
GuillaumeKfr <guillaume.kaiffer@sanofi.com>
2026-03-19 15:49:43 +0100

    fix(oil): disable C-l & C-h
    clashes with tmux pane navigation

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -13,6 +13,10 @@ return {
     config = function()
       require('oil').setup {
         view_options = { show_hidden = true },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<C-l>'] = false,
+        },
       }
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = '[-] Open parent directory' })
     end,
